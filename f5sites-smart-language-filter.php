300f6f1f4c8fe6ec103cc95506ccecbd4e00692d
Francisco Mat <francisco@f5sites.com>
2019-06-24 02:21:01 -0300

    smart plugin almost done

--- f5sites-smart-language-filter.php
+++ f5sites-smart-language-filter.php
@@ -7,9 +7,99 @@ Plugin Author: Francisco Mat
 Author URI: https://www.franciscomat.com/
 License: GPLv3
 */
+add_action('wp_loaded', 'smartlang_set_user_language');
+add_action('loop_start', 'smartlang_check_language_user_and_content');
+
+add_action('pre_get_posts', 'smartlang_filter_by_tag', 10, 2);
+
+add_filter('pre_get_document_title', 'smartlang_define_title_apendix');
+
+global $base_link;
+$base_link = $_SERVER['SERVER_NAME']; 
+$base_link = preg_replace('/\?.*/', '', $base_link);
+
+#function smartlang_filter_by_tag($post_object, $query) {
+function smartlang_filter_by_tag($query) {
+	#
+	#if(has_tag("lang-es", $post_object->ID))
+		#echo "AAAAA $post_object->ID";
+	#	return $post_object;
+	#else
+	#	wp_reset_postdata();
+
+	#return NULL;
+	#var_dump($query);die;
+	#echo "AQUI CHAMOU";
+	if ( $query->is_home() && $query->is_main_query() ) {
+		#echo "AQUI FILTROU";
+		#if(function_exists('set_shared_database_schema'))set_shared_database_schema();
+			#wp_reset_query();
+			global $base_link;
+			$idObj = get_category_by_slug($base_link); 
+
+			global $user_prefered_language;
+			
+			#$user_prefered_language_prefix = substr($user_prefered_language, 0,2);
+			global $user_prefered_language_prefix;
+			$arro = array(
+				'cat' => $idObj->term_id,
+				'posts_per_page' => 10,
+				'tag' => "lang-".$user_prefered_language_prefix,
+			);
+			
+			/*if($user_prefered_language=="en" || $user_prefered_language=="en_US")
+				#$args[] = array("tag"=>"english");
+				$arro["tag"] = "english";
+			else 
+				$arro["tag__not_in"] = 579;*/
+			#wp_reset_query();
+			#$catquery9 = new WP_Query( $arro );
+			#$query = $catquery9;
+		#$q = new WP_Query( 'posts_per_page=5' );
+		#$query = $q;
+		#$args = array(
+		#	'posts_per_page' => '5',
+		#);
+		#$query = new WP_Query($args);
+		
+		$query->set('posts_per_page', -1);
+		$taxquery = array(
+			array(            
+				'taxonomy' => 'post_tag',
+				'field' => 'slug',
+				#'terms' => "lang-es",
+				'terms' => "lang-".$user_prefered_language_prefix,
+			)
+		);      
+		$query->set('tax_query', $taxquery);
+		/*
+		$tax_query = array(
+				'taxonomy' => 'post_tag', 
+				'field' => 'slug', 
+				'terms' => "lang-".$user_prefered_language_prefix,
+            	'operator'=> 'IN' );
+		$query->tax_query->queries[] = $tax_query; 
+		$query->query_vars['tax_query'] = $query->tax_query->queries;
+		*/
+	}
+
+	/*$tax_query = array(
+				'taxonomy' => 'tag', 
+				'field' => 'slug', 
+				'terms' => "lang-".$user_prefered_language_prefix,
+            	'operator'=> 'IN' );
+	$query->tax_query->queries[] = $tax_query; 
+	$query->query_vars['tax_query'] = $query->tax_query->queries;
+*/
+	#var_dump($user_prefered_language);die;
+
+	#return $user_prefered_language;
+}
 
 function smartlang_set_user_language() {
+	#echo "~ASDASD";die;
 	global $user_prefered_language;
+	global $user_prefered_language_prefix;
 	session_start();
 
 	//Try WooCommerce geolocate
@@ -54,18 +144,23 @@ function smartlang_set_user_language() {
 		$user_prefered_language=="en_US";
 	
 	switch_to_locale($user_prefered_language);
-	smartlang_define_title_apendix($user_prefered_language);
+	smartlang_define_title_apendix();
 	
-	return $user_prefered_language;
+	$user_prefered_language_prefix = substr($user_prefered_language,0,2);
 }
 
-function smartlang_define_title_apendix($lang) {
+function smartlang_define_title_apendix() {
+	#$title .=' ok';
+	#return $title;
+	#smartlang_set_user_language();
+	global $user_prefered_language;
+	#var_dump("user_prefered_language". $user_prefered_language);die;
 	global $title_apendix;
-	switch ($lang) {
+	switch ($user_prefered_language) {
 		case 'notset' :
 		case 'en' :
 		case 'en_US' :
-			$title_apendix = "USA/UK";
+			$title_apendix = "USA / UK";
 			break;
 		case 'pt' :
 		case 'pt_BR' :
@@ -79,7 +174,7 @@ function smartlang_define_title_apendix($lang) {
 
 		case 'es' :
 		case 'es_ES' :
-			$title_apendix = "Espanã/AL";
+			$title_apendix = "Espanã / America Latina";
 			break;
 
 		case 'zh' :
@@ -90,9 +185,13 @@ function smartlang_define_title_apendix($lang) {
 			$title_apendix = "Global";
 			break;
 	}
+	//echo "adsdadsas".$title;
+	//die($title + $title_apendix);
+	$title = get_bloginfo('title')." - ".get_bloginfo('description');
+	return $title." | ".$title_apendix;
 }
 
-function smartlang_show_lang_options($hide_title=false, $current_location="") {
+function smartlang_show_lang_options($hide_title=false, $show_name=false, $current_location="") {
 	
 	global $user_prefered_language;
 	
@@ -117,36 +216,36 @@ function smartlang_show_lang_options($hide_title=false, $current_location="") {
 	#var_dump($user_prefered_language);
 	#var_dump($current_location);die;
 	switch ($current_location) {
-		smartlang_generate_flag_links_except($current_location);
-		/*case 'notset' :
+		//smartlang_generate_flag_links_except($current_location);
+		case 'notset' :
 		case 'en' :
 		case 'en_US' :
-			smartlang_generate_flag_links_except("en");
+			smartlang_generate_flag_links_except("en", $show_name);
 			break;
 		
 		case 'pt' :
 		case 'pt_BR' :
-			smartlang_generate_flag_links_except("pt");
+			smartlang_generate_flag_links_except("pt", $show_name);
 			break;
 
 		case 'fr' :
 		case 'fr_FR' :
-			smartlang_generate_flag_links_except("fr");
+			smartlang_generate_flag_links_except("fr", $show_name);
 			break;
 
 		case 'es' :
 		case 'es_ES' :
-			smartlang_generate_flag_links_except("es");
+			smartlang_generate_flag_links_except("es", $show_name);
 			break;
 
 		case 'zh' :
 		case 'zh_CN' :
-			smartlang_generate_flag_links_except("zh");
+			smartlang_generate_flag_links_except("zh", $show_name);
 			break;
 
 		default:
-			generate_flag_links_except("en");
-			break;*/
+			generate_flag_links_except("en", $show_name);
+			break;
 	}
 	/*
 	if($current_location!="en" && $current_location!="en_US") { ?>
@@ -155,43 +254,49 @@ function smartlang_show_lang_options($hide_title=false, $current_location="") {
 		<?php } else { ?>
 			<strong>Change Language:</strong>
 		<?php } ?>
-		<a href="<?php echo get_bloginfo('url'); ?>/?lang=en_US"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/flag-lang/us.png" alt="Language Flag"> English</a>
+		<a href="<?php echo get_bloginfo('url'); ?>/?lang=en_US"><img src="<?php echo plugin_dir_url( __FILE__ ) ?>flags/us.png" alt="Language Flag"> English</a>
 	<?php } else { ?>
 		<?php if($showtitle_in_h3) { ?>
 			<h3 class="widget-title">Mudar Idioma</h3>
 		<?php } else { ?>
 			<strong>Mudar Idioma:</strong>
 		<?php } ?>
-		<a href="<?php echo get_bloginfo('url'); ?>/?lang=pt_BR"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/flag-lang/br.png" alt="Bandeira de Idioma"> Português</a>
+		<a href="<?php echo get_bloginfo('url'); ?>/?lang=pt_BR"><img src="<?php echo plugin_dir_url( __FILE__ ) ?>flags/br.png" alt="Bandeira de Idioma"> Português</a>
 	<?php }*/
 }
 
-function smartlang_generate_flag_links_except($except) { ?>
+function smartlang_generate_flag_links_current($show_name) {
+	global $user_prefered_language_prefix;
+	 ?>
+			<img src="<?php echo plugin_dir_url( __FILE__ ) ?>flags/<?php echo $user_prefered_language_prefix; ?>.png" alt="Language Flag" style="display: inline;"> <?php if($show_name) echo "English";?>
+	<?php return;
+}
+
+function smartlang_generate_flag_links_except($except, $show_name) { ?>
 	<?php 
-	$base_link = $_SERVER['REQUEST_URI']; 
-	$base_link = preg_replace('/\?.*/', '', $base_link);
+		
 	?>
 	<?php if($except!="en" && $except!="en_US") { ?>
-		<a href="<?php echo $base_link; ?>?lang=en_US"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/flag-lang/us.png" alt="Language Flag"> English</a>
+		<a href="<?php echo $base_link; ?>?lang=en_US"><img src="<?php echo plugin_dir_url( __FILE__ ) ?>flags/en.png" alt="Language Flag"> <?php if($show_name) echo "English";?></a>
 	<?php } ?>
 	<?php if($except!="fr" && $except!="fr_FR") { ?>
-		<a href="<?php echo $base_link; ?>?lang=fr_FR"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/flag-lang/fr.png" alt="Drapeau de langue"> Français</a>
+		<a href="<?php echo $base_link; ?>?lang=fr_FR"><img src="<?php echo plugin_dir_url( __FILE__ ) ?>flags/fr.png" alt="Drapeau de langue"> <?php if($show_name) echo "Français";?></a>
 	<?php } ?>
 	<?php if($except!="pt" && $except!="pt_BR") { ?>
-		<a href="<?php echo $base_link; ?>?lang=pt_BR"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/flag-lang/br.png" alt="Bandeira de Idioma"> Português</a>
+		<a href="<?php echo $base_link; ?>?lang=pt_BR"><img src="<?php echo plugin_dir_url( __FILE__ ) ?>flags/pt.png" alt="Bandeira de Idioma"> <?php if($show_name) echo "Português";?></a>
 	<?php } ?>
 	<?php if($except!="es" && $except!="es_ES") { ?>
-		<a href="<?php echo $base_link; ?>?lang=es_ES"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/flag-lang/es.png" alt="Bandera de Idioma"> Spañol</a>
+		<a href="<?php echo $base_link; ?>?lang=es_ES"><img src="<?php echo plugin_dir_url( __FILE__ ) ?>flags/es.png" alt="Bandera de Idioma"> <?php if($show_name) echo "Spañol";?></a>
 	<?php } ?>
 	<?php if($except!="zh" && $except!="zh_CN") { ?>
-		<a href="<?php echo $base_link; ?>?lang=zh_CN"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/flag-lang/cn.png" alt="语言标志"> 中文</a>
+		<a href="<?php echo $base_link; ?>?lang=zh_CN"><img src="<?php echo plugin_dir_url( __FILE__ ) ?>flags/cn.png" alt="语言标志"> <?php if($show_name) echo "中文";?></a>
 	<?php } ?>
 <?php }
 
 function smartlang_check_language_user_and_content($tags) {
 	global $user_prefered_language;
 	global $user_prefered_language_prefix;
-	$user_prefered_language_prefix = substr($user_prefered_language,0,2);
+	
 	if($tags) {
 		foreach ($tags as $tag) {
 			# code...
@@ -203,7 +308,7 @@ function smartlang_check_language_user_and_content($tags) {
 					<?php _e("These content is not avaiable in your language. Original content language is: ", "sis-foca-js");
 					echo "<strong>".$content_lang."</strong>";
 					echo "<br>";
-					echo "<a href='/'>".__("Go to blog homepage", "sis-foca-js")."</a>";
+					echo "<a href='/'>".__("Go to blog homepage", "sis-foca-js")." IN YOUR LANGUAGE | IN CURRENTE LANGUAGE</a>";
 					#echo "These content is not avaiable in your language";	] ?>
 					</div>
 				<?php }
@@ -211,4 +316,47 @@ function smartlang_check_language_user_and_content($tags) {
 		}
 	}
 	#var_dump($user_prefered_language_prefix);die;
+}
+
+function smartlang_recent_posts_georefer_widget() {
+	#<h3><script>document.write(txt_foot_blog)</script></h3>
+	?>
+	<div class="widget DDDwidget_recent_entries">
+		<ul style="list-style: none;">
+		
+			<?php 
+			global $base_link;
+			#if(function_exists('set_shared_database_schema'))set_shared_database_schema();
+			#echo $base_link;die;
+			$idObj = get_category_by_slug($base_link); 
+
+			global $user_prefered_language;
+			
+			#$user_prefered_language_prefix = substr($user_prefered_language, 0,2);
+			global $user_prefered_language_prefix;
+			$arro = array(
+				'cat' => $idObj->term_id,
+				'posts_per_page' => 10,
+				'tag' => "lang-".$user_prefered_language_prefix,
+			);
+			
+			/*if($user_prefered_language=="en" || $user_prefered_language=="en_US")
+				#$args[] = array("tag"=>"english");
+				$arro["tag"] = "english";
+			else 
+				$arro["tag__not_in"] = 579;*/
+			wp_reset_query();
+			$catquery = new WP_Query( $arro );
+			while($catquery->have_posts()) : $catquery->the_post();
+			?>
+			
+			<li>
+				<?php the_post_thumbnail(array(50,50)); ?>
+				<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+			</li>
+			
+			<?php endwhile;	?>
+		</ul>
+	</div>
+	<?php
 }
